96e2c64ee862e38d2b403a2f7e406ce232b8475f
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-10-02 11:50:28 +0900

    [change]

---
+++ src/components/Detail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import {View, Text, Image, Button} from 'react-native';
+
+const Detail: React.FC = ({navigation, route}) => {
+  const {id, userIcon, userName, userId, detail, image} = route.params.item;
+  console.log(detail);
+  return (
+    <View>
+      <Text>hello</Text>
+      <Button title="Back to Home" onPress={() => navigation.goBack()} />
+    </View>
+  );
+};
+export default Detail;
