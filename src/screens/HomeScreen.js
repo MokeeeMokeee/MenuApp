4f16e4a355c3b7fa16fb046376e7485bc91e66fc
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-10-02 11:51:04 +0900

    [add] home screen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {SafeAreaView, FlatList, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import TimeLine from '../components/TimeLine';
+import Detail from '../components/Detail';
+
+const HomeStack = createStackNavigator();
+
+const HomeScreen: React.FC = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="TimeLine"
+        component={TimeLine}
+        options={{
+          headerTitle: 'タイムライン',
+        }}
+      />
+      <HomeStack.Screen
+        name="Detail"
+        component={Detail}
+        options={{
+          title: '詳細ページ',
+        }}
+      />
+    </HomeStack.Navigator>
+  );
+};
+
+export default HomeScreen;
